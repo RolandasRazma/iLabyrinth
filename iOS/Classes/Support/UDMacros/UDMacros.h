e1e00a38022e1eb2f9bd88f1f340f8a884e38344
Rolandas Razma <rolandas@razma.lt>
2012-01-18 10:39:08 +0200

    removed unused macros

--- iOS/Classes/Support/UDMacros/UDMacros.h
+++ iOS/Classes/Support/UDMacros/UDMacros.h
@@ -5,10 +5,6 @@
 //  Copyright 2009 UD7. All rights reserved.
 //
 
-#define _(format, ...) [NSString stringWithFormat: [[NSBundle mainBundle] localizedStringForKey:format value:@"" table:nil] , ##__VA_ARGS__]
-#define MAX3(A,B,C) MAX(MAX(A, B), C)
-
-
 static BOOL isDeviceIPad(){
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 30200
     if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ) {
